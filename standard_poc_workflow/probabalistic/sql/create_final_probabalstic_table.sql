10f8f83f7b108fe2f3215ee4a4a36d153e52e99e
Vishal Patel <vishalpatel2890@gmail.com>
2024-12-17 08:23:30 -0500

    update prob

--- standard_poc_workflow/probabalistic/sql/create_final_probabalstic_table.sql
+++ standard_poc_workflow/probabalistic/sql/create_final_probabalstic_table.sql
@@ -1,9 +1,13 @@
 SELECT 
-   *
+   unification_id, 
+   MAX_BY(cluster_id, avg_cluster_similarity) as final_cluster_id, 
+   
 FROM
   prob_final_cluster
+GROUP BY  
+  unification_id
 WHERE
   avg_cluster_similarity >= 0.8
-ORDER BY
-  avg_cluster_similarity,
-  cluster_id
+-- ORDER BY
+--   avg_cluster_similarity,
+--   cluster_id
